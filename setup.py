e400d290f1e96d5cdfb88d7a8d2f7d95db67115a
nakish13 <78470594+nakish13@users.noreply.github.com>
2021-02-05 15:41:07 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from waitress import serve
 from flask_app import create_app 
-
+import os
+port_= int(os.environ.get("PORT", 5000))
 app=create_app()
 
 if __name__ == '__main__':
+	
     app.run()         
-    serve(app)
+    serve(app,port=port_)
